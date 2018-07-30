27b01ffa09ba09933fbe0e021d6af444d8f2f069
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-07-30 01:16:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class application
   def call(env)
     resp = Rack::Response.new
     now = Time.now
-    if datetime <= 12:00
+    if now <= 12:00
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
