3304382f71364ae288aac5227c532fcf9e34eb56
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-07-30 01:13:56 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if self.time <= 12:00
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
